537eb3e91852ebbcd89e96ff2280fa2bfb965c87
buildevol <buildevol@outlook.com>
2020-05-14 21:22:28 +0800

    Updates

---
+++ precache-manifest.43e9006fa2dbbdfb89228ce9bd1e4f10.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e917984f3578e96a525a6b653c539a9b",
+    "url": "/smartbrains/index.html"
+  },
+  {
+    "revision": "0aae0a9bb2d4c40e51c7",
+    "url": "/smartbrains/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "080ab9f73e8be8645f76",
+    "url": "/smartbrains/static/css/main.cd384c93.chunk.css"
+  },
+  {
+    "revision": "0aae0a9bb2d4c40e51c7",
+    "url": "/smartbrains/static/js/2.84ef4563.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/smartbrains/static/js/2.84ef4563.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "080ab9f73e8be8645f76",
+    "url": "/smartbrains/static/js/main.5ef1ab81.chunk.js"
+  },
+  {
+    "revision": "cf6c4092425e97fa1d09",
+    "url": "/smartbrains/static/js/runtime-main.0038ac4c.js"
+  }
+]);
