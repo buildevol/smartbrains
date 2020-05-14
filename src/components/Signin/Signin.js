60992f42f26b35ac853f511ebc38953feeec0294
buildevol <buildevol@outlook.com>
2020-05-14 20:41:32 +0800

    Resolved url link

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -18,7 +18,7 @@ class Signin extends React.Component {
   };
 
   onSubmitSignIn = () => {
-    fetch("https://shrouded-cliffs-55433.herokuapp.com/", {
+    fetch("https://shrouded-cliffs-55433.herokuapp.com/signin", {
       method: "post",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
